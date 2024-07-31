28b7cbbcf8bc94d0efbecb06130d143348c1188c
EvgenPetrov <bradar748@gmail.com>
2024-07-31 11:34:20 +0300

    Начальные компоненты

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
+import Drawer from "./components/Drawer";
+import Header from "./components/Header";
+
 function App() {
     return (
         <div className="wrapper clear">
-            <header className="d-flex justify-between align-center p-40">
-                <div className="d-flex align-center">
-                    <img width={40} height={40} src="/img/logo.png" alt="" />
-                    <div>
-                        <h3 className="text-uppercase">React sneakers</h3>
-                        <p className="opacity-5тз">Магазин лучших кроссовок</p>
+            <Drawer />
+
+            <Header />
+
+            <div className="content p-40">
+                <div className="d-flex align-center justify-between mb-40">
+                    <h1>Все кроссовки</h1>
+                    <div className="search-block d-flex">
+                        <img src="/img/search.svg" alt="Search" />
+                        <input placeholder="Поиск..." />
                     </div>
                 </div>
-                <ul className="d-flex">
-                    <li className="mr-30">
-                        <img width={18} height={18} src="/img/cart.svg" alt="" />
-                        <span>1205 руб.</span>
-                    </li>
-                    <li>
-                        <img width={18} height={18} src="/img/user.svg" alt="" />
-                    </li>
-                </ul>
-            </header>
-            <div className="content p-40">
-                <h1 className="mb-40">Все кроссовки</h1>
 
                 <div className="d-flex">
                     <div className="card">
+                        <div className="favorite">
+                            <img src="/img/unliked.svg" alt="Unliked" />
+                        </div>
                         <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
                         <h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
                         <div className="d-flex justify-between align-center">
